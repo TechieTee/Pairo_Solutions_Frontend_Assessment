b65ef68d3a3bd16260299148c06cef84cacfde81
techietee <mailtomodupe@gmail.com>
2022-08-23 22:00:43 +0100

    installed react-router-dom, set routes and added pages and component folder

---
+++ src/pages/CompletePurchase.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const completePurchase = () => {
+    return (
+        <div>
+            completePurchase
+        </div>
+    );
+};
+
+export default completePurchase;
